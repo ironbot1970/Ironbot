6d768bd403ef48929dad55bc67d7951b2459408d
Peter Kiraly <peter.kiral@gmail.com>
2023-12-10 17:58:20 +0100

    Frontend Base

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/partial/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/pages/home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SearchComponent } from './components/partial/search/search.component';
+import { TagsComponent } from './components/partial/tags/tags.component';
+import { RobotPageComponent } from './components/pages/robot-page/robot-page.component';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { TitleComponent } from './components/partial/title/title.component';
+import { NotFoundComponent } from './components/partial/not-found/not-found.component';
+// import { RatingModule } from 'ng-starrating';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SearchComponent,
+    TagsComponent,
+    RobotPageComponent,
+    CartPageComponent,
+    TitleComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    // RatingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/src/app/components/pages/robot-page/robot-page.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
+import { RobotService } from 'src/app/services/robot.service';
+import { Robot } from 'src/app/shared/models/Robot';
+
+@Component({
+  selector: 'app-robot-page',
+  templateUrl: './robot-page.component.html',
+  styleUrls: ['./robot-page.component.css']
+})
+export class RobotPageComponent implements OnInit{
+  
+  robot!:Robot;
+  constructor(activatedRoute:ActivatedRoute, robotService:RobotService,
+    private cartService:CartService, private router: Router){
+    activatedRoute.params.subscribe((params) => {
+      if(params.id)
+      this.robot = robotService.getRobotById(params.id);
+    })
+  }
+  
+  ngOnInit(): void {
+    
+  }
+
+  addToCart(){
+    this.cartService.addToCart(this.robot);
+    this.router.navigateByUrl('/cart-page');
+  }
+}
